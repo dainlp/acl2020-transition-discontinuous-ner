a6012a9f33c459102d4fbe503c43f5f3da9de30c
Dai <dai.xiang.au@gmail.com>
2020-05-27 21:44:07 +0200

    update preprocessing code

--- data/cadec/extract_annotations.py
+++ data/cadec/extract_annotations.py
@@ -12,9 +12,10 @@ def parse_parameters(parser=None):
     if parser is None: parser = argparse.ArgumentParser()
 
     ## Required
-    parser.add_argument("--input_ann", default="/data/dai031/Corpora/CADEC/cadec/original-fixed", type=str)
+    parser.add_argument("--input_ann", default="/data/dai031/Corpora/CADEC/cadec/original", type=str)
     parser.add_argument("--input_text", default="/data/dai031/Corpora/CADEC/cadec/text", type=str)
-    parser.add_argument("--output_filepath", default="/data/dai031/Experiments/CADEC/ann", type=str)
+    parser.add_argument("--output_filepath", default="/data/dai031/Experiments/CADEC/all/ann", type=str)
+    parser.add_argument("--type_of_interest", default="")
 
     args, _ = parser.parse_known_args()
     return args
@@ -23,20 +24,20 @@ def parse_parameters(parser=None):
 def _get_mention_from_text(text, indices):
     tokens = []
     for i in range(0, len(indices), 2):
-        tokens.append(text[int(indices[i]):int(indices[i + 1])])
-    tokens = [t.strip() for t in tokens if len(t.strip()) > 0]
+        start = int(indices[i])
+        end = int(indices[i + 1])
+        tokens.append(text[start:end])
     return " ".join(tokens)
 
 
 if __name__ == "__main__":
     args = parse_parameters()
-    num_mentions = 0
-
+    num_annotations = 0
     with open(args.output_filepath, "w") as out_f:
-        for doc in os.listdir(args.input_ann):
-            with open(os.path.join(args.input_ann, doc), "r") as in_f:
-                doc = doc.replace(".ann", "")
-                text = open(os.path.join(args.input_text, "%s.txt" % doc)).read()
+        for document in os.listdir(args.input_ann):
+            with open(os.path.join(args.input_ann, document), "r") as in_f:
+                document = document.replace(".ann", "")
+                text = open(os.path.join(args.input_text, "%s.txt" % document)).read()
                 for line in in_f:
                     line = line.strip()
                     if line[0] != "T": continue
@@ -45,14 +46,13 @@ if __name__ == "__main__":
                     mention = sp[2]
                     sp = sp[1].split(" ")
                     label = sp[0]
+                    if args.type_of_interest != "" and args.type_of_interest != label: continue
                     indices = extract_indices_from_brat_annotation(" ".join(sp[1:]))
                     mention_from_text = _get_mention_from_text(text, indices)
                     if mention != mention_from_text:
-                        if sorted(mention.split()) != sorted(mention_from_text.split()):
-                            print("Update the mention in document %s from (%s) to (%s)." % (
-                            doc, mention, mention_from_text))
+                        print("Update the mention from (%s) to (%s)." % (mention, mention_from_text))
                         mention = mention_from_text
-                    num_mentions += 1
-                    out_f.write("%s\t%s\t%s\t%s\n" % (doc, label, ",".join([str(i) for i in indices]), mention))
+                    num_annotations += 1
+                    out_f.write("%s\t%s\t%s\t%s\n" % (document, label, ",".join([str(i) for i in indices]), mention))
 
-    print("Extract %d annotations." % num_mentions)
+    print("Extract %d annotations." % num_annotations)
